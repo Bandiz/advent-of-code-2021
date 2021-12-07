f6b07e2ea707c2dd34bf711a35accd85fd185a82
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-12-07 13:23:48 +0200

    added day 7 part 2 solution

--- 7 - The Treachery of Whales/main.js
+++ 7 - The Treachery of Whales/main.js
@@ -20,15 +20,15 @@ const readline = require("readline");
 
   await events.once(rl, "close");
 
-  function calculateAlignmentFuel() {
+  function calculateAlignmentFuelPart1() {
     let fuel = Number.MAX_VALUE;
 
     for (const position in crabsMap) {
       let currentFuel = 0;
       for (const position2 in crabsMap) {
-        const crabCount2 = crabsMap[position2];
+        const crabCount = crabsMap[position2];
         const positionDiff = Math.abs(position2 - position);
-        currentFuel += positionDiff * crabCount2;
+        currentFuel += positionDiff * crabCount;
       }
       if (fuel > currentFuel) {
         fuel = currentFuel;
@@ -37,8 +37,30 @@ const readline = require("readline");
     return fuel;
   }
 
-  let resultPart1 = calculateAlignmentFuel();
-  let resultPart2 = 0;
+  function calculateAlignmentFuelPart2() {
+    let fuel = Number.MAX_VALUE;
+    const keys = Object.keys(crabsMap).map(Number);
+
+    for (let position = keys[0]; position < keys[keys.length - 1]; position++) {
+      let currentFuel = 0;
+      for (const position2 in crabsMap) {
+        const crabCount = crabsMap[position2];
+        const positionDiff = Math.abs(position2 - position);
+        let accumulateFuel = 0;
+        for (let i = 1; i <= positionDiff; i++) {
+          accumulateFuel += i;
+        }
+        currentFuel += accumulateFuel * crabCount;
+      }
+      if (fuel > currentFuel) {
+        fuel = currentFuel;
+      }
+    }
+    return fuel;
+  }
+
+  let resultPart1 = calculateAlignmentFuelPart1();
+  let resultPart2 = calculateAlignmentFuelPart2();
 
   fs.writeFileSync(
     "data.out",
