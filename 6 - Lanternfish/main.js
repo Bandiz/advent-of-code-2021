aa5230111aaaa6fa200dd195f8e5f5f7d76844a3
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-12-07 12:12:21 +0200

    added calculations day 6 part 2; calculations with javascript are not accurate although algorithm is correct

--- 6 - Lanternfish/main.js
+++ 6 - Lanternfish/main.js
@@ -9,51 +9,31 @@ const readline = require("readline");
     terminal: false,
   });
 
-  class Lanternfish {
-    #cycle;
-    constructor(cycle = 8) {
-      this.#cycle = Number(cycle);
-    }
-
-    advanceDay() {
-      if (this.#cycle == 0) {
-        this.#cycle = 6;
-      } else {
-        this.#cycle--;
-      }
-    }
-
-    createFish() {
-      if (this.#cycle == 0) {
-        return new Lanternfish();
-      }
-      return null;
-    }
-  }
-
-  let fishes;
+  let fishQueue = new Array(9).fill(0);
 
   rl.on("line", (line) => {
-    fishes = line.split(",").map(x => new Lanternfish(x));
+    let fishes = line.split(",").map(Number);
+    for (let i = 0; i < fishes.length; i++) {
+      fishQueue[fishes[i]]++;
+    }
   });
 
-  function simulatePart1(currentDay, endDay, fishes) {
-    if (currentDay == endDay) return fishes.length;
-    let deltaFish = [];
-    for (let i = 0; i < fishes.length; i++) {
-      let fish = fishes[i];
-      deltaFish.push(fish);
-      const newFish = fish.createFish();
-      if (newFish) deltaFish.push(newFish);
-      fish.advanceDay();
+  function simulatePopulationGrowth(days) {
+    for (let day = 0; day < days; day++) {
+      const newFish = fishQueue.shift();
+      fishQueue.push(0);
+     
+      fishQueue[6] += newFish;
+      fishQueue[8] = newFish;   
     }
-    return simulatePart1(++currentDay, endDay, deltaFish);
+
+    return fishQueue.reduce((prev, curr) => prev + curr, 0);
   }
 
   await events.once(rl, "close");
 
-  let resultPart1 = simulatePart1(0, 80, fishes);
-  let resultPart2 = 0;
+  let resultPart1 = simulatePopulationGrowth(80);
+  let resultPart2 = simulatePopulationGrowth(256);
 
   fs.writeFileSync(
     "data.out",
