414a5f853e5c0d86c5e70ec21a0e56448cd6baa8
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-12-14 23:35:25 +0200

    updated part 1 to be more efficient; added solution for day 14 part 2

--- 14 - Extended Polymerization/main.js
+++ 14 - Extended Polymerization/main.js
@@ -9,7 +9,7 @@ const readline = require("readline");
     terminal: false,
   });
 
-  let polymerTemplate = [];
+  let polymerTemplate;
   const pairInsertions = {};
   let separator = 0;
 
@@ -20,38 +20,49 @@ const readline = require("readline");
       const [key, value] = line.split(" -> ");
       pairInsertions[key] = value;
     } else {
-      polymerTemplate.push(...line.split(""));
+      polymerTemplate = line;
     }
   });
 
   await events.once(rl, "close");
 
-  function calculateOccurances(polymerTemplate, pairInsertions, steps = 10) {
-    let stepSequence = [...polymerTemplate];
+  function calculateOccurances(polymerTemplate, pairInsertions, steps) {
+    let pairSum = {};
+
+    for (let x1 = 0, x2 = 1; x2 < polymerTemplate.length; x1++, x2++) {
+      const polymer = polymerTemplate[x1] + polymerTemplate[x2];
+      pairSum[polymer] = ++pairSum[polymer] || 1;
+    }
 
     for (let step = 0; step < steps; step++) {
-      for (let x1 = 0, x2 = 1; x2 < stepSequence.length; x1++, x2++) {
-        const combination = `${stepSequence[x1]}${stepSequence[x2]}`;
-        const insertion = pairInsertions[combination];
-        if (typeof insertion !== "undefined") {
-          stepSequence.splice(x2, 0, insertion);
-          x1++;
-          x2++;
-        }
+      const newPairSum = {};
+      for (const [key, value] of Object.entries(pairSum)) {
+        const insertion = pairInsertions[key];
+        const pair1 = key[0] + insertion,
+          pair2 = insertion + key[1];
+        newPairSum[pair1] = (newPairSum[pair1] ?? 0) + value;
+        newPairSum[pair2] = (newPairSum[pair2] ?? 0) + value;
       }
+      pairSum = newPairSum;
     }
-    const lookup = stepSequence.reduce((prev, curr) => {
-      prev[curr] = ++prev[curr] || 1;
+    console.log(pairSum);
+
+    const seqSum = Object.entries(pairSum).reduce((prev, [key, value]) => {
+      prev[key[0]] = (prev[key[0]] ?? 0) + value;
+      prev[key[1]] = (prev[key[1]] ?? 0) + value;
       return prev;
     }, {});
-    const values = Object.values(lookup);
-    const min = values.reduce((a, b) => Math.min(a, b));
-    const max = values.reduce((a, b) => Math.max(a, b));
+
+    console.log(seqSum);
+
+    const values = Object.values(seqSum);
+    const min = Math.ceil(values.reduce((a, b) => Math.min(a, b)) / 2);
+    const max = Math.ceil(values.reduce((a, b) => Math.max(a, b)) / 2);
     return max - min;
   }
 
-  let resultPart1 = calculateOccurances(polymerTemplate, pairInsertions);
-  let resultPart2 = 0;
+  let resultPart1 = calculateOccurances(polymerTemplate, pairInsertions, 10);
+  let resultPart2 = calculateOccurances(polymerTemplate, pairInsertions, 40);
 
   fs.writeFileSync(
     "data.out",
