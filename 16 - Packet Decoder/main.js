bf501d32c1861a6ec5ec480ea22dc3560c689a61
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-12-16 21:14:15 +0200

    refactoring for day 16

--- 16 - Packet Decoder/main.js
+++ 16 - Packet Decoder/main.js
@@ -4,7 +4,7 @@ const readline = require("readline");
 
 (async () => {
   const rl = readline.createInterface({
-    input: fs.createReadStream("data1.in"),
+    input: fs.createReadStream("data.in"),
     output: process.stdout,
     terminal: false,
   });
@@ -26,110 +26,102 @@ const readline = require("readline");
 
   await events.once(rl, "close");
 
-  function calculateVersionNumbers(packet) {
-    const packetTree = formPacketTree(packet);
+  function findSubPacketLength(startIndex, packet, packets) {
+    let subIndex = startIndex;
+    for (let subPacket = 0; subPacket < packets; subPacket++) {
+      subIndex += 3;
+      const subtype = parseInt(packet.substring(subIndex, (subIndex += 3)), 2);
+      switch (subtype) {
+        case 4:
+          let isLast = false;
+          while (!isLast) {
+            if (packet[subIndex] === "0") {
+              isLast = true;
+            }
+            subIndex += 5;
+          }
+          break;
+        default:
+          const subLengthIndex = packet[subIndex++] == "1";
+          const subLength = parseInt(
+            packet.substring(subIndex, (subIndex += subLengthIndex ? 11 : 15)),
+            2
+          );
+          if (!subLengthIndex) {
+            subIndex += subLength;
+          } else {
+            subIndex += findSubPacketLength(subIndex, packet, subLength);
+          }
+          break;
+      }
+    }
+    return subIndex - startIndex;
+  }
 
+  function formPacketTree(packet) {
+    const adjacent = [];
+
+    let i = 0;
+    while (i + 7 < packet.length) {
+      const version = parseInt(packet.substring(i, (i += 3)), 2);
+      const type = parseInt(packet.substring(i, (i += 3)), 2);
+      const newPacket = {
+        version,
+        type,
+      };
+
+      switch (type) {
+        case 4: {
+          let isLast = false;
+          const numbers = [];
+          while (!isLast) {
+            if (packet[i] === "0") {
+              isLast = true;
+            }
+            numbers.push(parseInt(packet.substring(i + 1, (i += 5)), 2));
+          }
+          newPacket.literal = Number.parseInt(numbers.join(""));
+          break;
+        }
+        default:
+          const lengthIndex = packet[i++] == "1";
+          let length = parseInt(
+            packet.substring(i, (i += lengthIndex ? 11 : 15)),
+            2
+          );
+          if (lengthIndex) {
+            const subIndex = findSubPacketLength(i, packet, length);
+
+            newPacket.subPackets = formPacketTree(
+              packet.substring(i, (i += subIndex))
+            );
+          } else {
+            newPacket.subPackets = formPacketTree(
+              packet.substring(i, (i += length))
+            );
+          }
+          break;
+      }
+      adjacent.push(newPacket);
+    }
+
+    return adjacent;
+  }
+
+  function calculateVersionNumbers(packetTree) {
     return packetTree.reduce((prev, curr) => prev + sumDeep(curr), 0);
 
     function sumDeep(node) {
       return (
         node.version +
-        node.subPackets.reduce((prev, curr) => prev + sumDeep(curr), 0)
+        (node.subPackets ?? []).reduce((prev, curr) => prev + sumDeep(curr), 0)
       );
-    }
-
-    function findSubPacketLength(startIndex, packet, packets) {
-      let subIndex = startIndex;
-      for (let subPacket = 0; subPacket < packets; subPacket++) {
-        subIndex += 3;
-        const subtype = parseInt(
-          packet.substring(subIndex, (subIndex += 3)),
-          2
-        );
-        switch (subtype) {
-          case 4:
-            let isLast = false;
-            while (!isLast) {
-              if (packet[subIndex] === "0") {
-                isLast = true;
-              }
-              subIndex += 5;
-            }
-            break;
-          default:
-            const subLengthIndex = packet[subIndex++] == "1";
-            const subLength = parseInt(
-              packet.substring(
-                subIndex,
-                (subIndex += subLengthIndex ? 11 : 15)
-              ),
-              2
-            );
-            if (!subLengthIndex) {
-              subIndex += subLength;
-            } else {
-              subIndex += findSubPacketLength(subIndex, packet, subLength);
-            }
-            break;
-        }
-      }
-      return subIndex - startIndex;
-    }
-
-    function formPacketTree(packet) {
-      const adjacent = [];
-
-      let i = 0;
-      while (i + 7 < packet.length) {
-        const version = parseInt(packet.substring(i, (i += 3)), 2);
-        const type = parseInt(packet.substring(i, (i += 3)), 2);
-        const newPacket = {
-          version,
-          type,
-          subPackets: [],
-        };
-
-        switch (type) {
-          case 4: {
-            let isLast = false;
-            const numbers = [];
-            while (!isLast) {
-              if (packet[i] === "0") {
-                isLast = true;
-              }
-              numbers.push(parseInt(packet.substring(i + 1, (i += 5)), 2));
-            }
-            newPacket.literal = numbers.join("");
-            break;
-          }
-          default:
-            const lengthIndex = packet[i++] == "1";
-            let length = parseInt(
-              packet.substring(i, (i += lengthIndex ? 11 : 15)),
-              2
-            );
-
-            if (lengthIndex) {
-              const subIndex = findSubPacketLength(i, packet, length);
-
-              newPacket.subPackets.push(
-                ...formPacketTree(packet.substring(i, (i += subIndex)))
-              );
-            } else {
-              newPacket.subPackets.push(
-                ...formPacketTree(packet.substring(i, (i += length)))
-              );
-            }
-            break;
-        }
-        adjacent.push(newPacket);
-      }
-
-      return adjacent;
     }
   }
 
-  let resultPart1 = calculateVersionNumbers(packet);
+  const tree = formPacketTree(packet);
+
+  let resultPart1 = calculateVersionNumbers(tree);
   let resultPart2 = 0;
 
   fs.writeFileSync(
