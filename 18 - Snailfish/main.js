d2f3eed7b598d90300dee4774a31003550c408a5
Linas Jakseboga <linas.jakseboga@gmail.com>
2021-12-20 10:34:22 +0200

    moved out functions; added solution for day 18 part 2

--- 18 - Snailfish/main.js
+++ 18 - Snailfish/main.js
@@ -18,182 +18,181 @@ const readline = require("readline");
 
   await events.once(rl, "close");
 
-  function sumSnailfishNumbers(numbers) {
-    function combineNumbers(number1, number2) {
-      if (number1.length == 0) {
-        return number2;
-      }
-      const newPair = [number1, number2];
-      number1.push(newPair);
-      number2.push(newPair);
+  function combineNumbers(number1, number2) {
+    if (number1.length == 0) {
+      return number2;
+    }
+    const newPair = [number1, number2];
+    number1.push(newPair);
+    number2.push(newPair);
 
-      return newPair;
+    return newPair;
+  }
+
+  function sumNumbers(pair) {
+    while (true) {
+      let exploded = checkDepth(pair);
+
+      if (exploded) {
+        continue;
+      }
+      let wasSplit = split(pair);
+
+      if (wasSplit) {
+        continue;
+      }
+      if (!exploded && !wasSplit) break;
+    }
+    return pair;
+  }
+
+  function explode(pair) {
+    const [left, right, parent] = pair;
+
+    explodeLeft(pair, left);
+    explodeRight(pair, right);
+
+    if (pair === parent[0]) {
+      parent.shift();
+      parent.unshift(0);
+    } else {
+      parent.splice(1, 1, 0);
     }
 
-    const sum = numbers.reduce(
+    function explodeLeft(parent, value) {
+      const [, , parentParent] = parent;
+
+      if (typeof parentParent === "undefined") {
+        return;
+      }
+
+      const [left, right] = parentParent;
+
+      switch (parent) {
+        case left:
+          if (typeof left === "number") {
+            parentParent[0] += value;
+          } else {
+            explodeLeft(parentParent, value);
+          }
+          break;
+        case right:
+          if (typeof right === "number") {
+            parentParent[1] += value;
+          } else if (typeof parentParent[0] === "number") {
+            parentParent[0] += value;
+          } else {
+            addDeepRight(parentParent[0], value);
+          }
+          break;
+      }
+    }
+
+    function explodeRight(parent, value) {
+      const [, , parentParent] = parent;
+
+      if (typeof parentParent === "undefined") {
+        return;
+      }
+
+      const [left, right] = parentParent;
+
+      switch (parent) {
+        case right:
+          if (typeof right === "number") {
+            parentParent[1] += value;
+          } else {
+            explodeRight(parentParent, value);
+          }
+          break;
+        case left:
+          if (typeof left === "number") {
+            parentParent[0] += value;
+          } else if (typeof parentParent[1] === "number") {
+            parentParent[1] += value;
+          } else {
+            addDeepLeft(parentParent[1], value);
+          }
+          break;
+      }
+    }
+
+    function addDeepLeft(parentParent, value) {
+      if (typeof parentParent === "undefined") return;
+      const [left] = parentParent;
+      if (typeof left === "object") {
+        addDeepLeft(left, value);
+      } else {
+        parentParent[0] += value;
+      }
+    }
+
+    function addDeepRight(parentParent, value) {
+      if (typeof parentParent === "undefined") return;
+      const [, right] = parentParent;
+      if (typeof right === "object") {
+        addDeepRight(right, value);
+      } else {
+        parentParent[1] += value;
+      }
+    }
+  }
+
+  function split(pair) {
+    const [left, right] = pair;
+    let splitOccured = false;
+
+    if (typeof left === "object") {
+      splitOccured = split(left);
+    } else if (left > 9) {
+      pair[0] = createPair(left, pair);
+      return true;
+    }
+
+    if (splitOccured) return true;
+
+    if (typeof right === "object") {
+      splitOccured = split(right);
+    } else if (right > 9) {
+      pair[1] = createPair(right, pair);
+      return true;
+    }
+    return splitOccured;
+
+    function createPair(value, parent) {
+      const splitLeft = Math.floor(value / 2);
+      const splitRight = Math.round(value / 2);
+      const newPair = [splitLeft, splitRight, parent];
+      return newPair;
+    }
+  }
+
+  function checkDepth(pair, depth = 1) {
+    if (depth == 5) {
+      explode(pair);
+      return true;
+    }
+
+    const [left, right] = pair;
+    let exploded = false;
+
+    if (typeof left === "object") {
+      exploded = checkDepth(left, depth + 1);
+    }
+
+    if (exploded) return true;
+
+    if (typeof right === "object") {
+      exploded = checkDepth(right, depth + 1);
+    }
+    return exploded;
+  }
+
+  function sumSnailfishNumbers(numbers) {
+    return numbers.reduce(
       (prev, curr) => sumNumbers(combineNumbers(prev, curr)),
       []
     );
-    return sum;
-
-    function sumNumbers(pair) {
-      while (true) {
-        let exploded = checkDepth(pair);
-
-        if (exploded) {
-          continue;
-        }
-        let wasSplit = split(pair);
-
-        if (wasSplit) {
-          continue;
-        }
-        if (!exploded && !wasSplit) break;
-      }
-      return pair;
-    }
-
-    function explode(pair) {
-      const [left, right, parent] = pair;
-
-      explodeLeft(pair, left);
-      explodeRight(pair, right);
-
-      if (pair === parent[0]) {
-        parent.shift();
-        parent.unshift(0);
-      } else {
-        parent.splice(1, 1, 0);
-      }
-
-      function explodeLeft(parent, value) {
-        const [, , parentParent] = parent;
-
-        if (typeof parentParent === "undefined") {
-          return;
-        }
-
-        const [left, right] = parentParent;
-
-        switch (parent) {
-          case left:
-            if (typeof left === "number") {
-              parentParent[0] += value;
-            } else {
-              explodeLeft(parentParent, value);
-            }
-            break;
-          case right:
-            if (typeof right === "number") {
-              parentParent[1] += value;
-            } else if (typeof parentParent[0] === "number") {
-              parentParent[0] += value;
-            } else {
-              addDeepRight(parentParent[0], value);
-            }
-            break;
-        }
-      }
-
-      function explodeRight(parent, value) {
-        const [, , parentParent] = parent;
-
-        if (typeof parentParent === "undefined") {
-          return;
-        }
-
-        const [left, right] = parentParent;
-
-        switch (parent) {
-          case right:
-            if (typeof right === "number") {
-              parentParent[1] += value;
-            } else {
-              explodeRight(parentParent, value);
-            }
-            break;
-          case left:
-            if (typeof left === "number") {
-              parentParent[0] += value;
-            } else if (typeof parentParent[1] === "number") {
-              parentParent[1] += value;
-            } else {
-              addDeepLeft(parentParent[1], value);
-            }
-            break;
-        }
-      }
-
-      function addDeepLeft(parentParent, value) {
-        if (typeof parentParent === "undefined") return;
-        const [left] = parentParent;
-        if (typeof left === "object") {
-          addDeepLeft(left, value);
-        } else {
-          parentParent[0] += value;
-        }
-      }
-
-      function addDeepRight(parentParent, value) {
-        if (typeof parentParent === "undefined") return;
-        const [, right] = parentParent;
-        if (typeof right === "object") {
-          addDeepRight(right, value);
-        } else {
-          parentParent[1] += value;
-        }
-      }
-    }
-
-    function split(pair) {
-      const [left, right] = pair;
-      let splitOccured = false;
-
-      if (typeof left === "object") {
-        splitOccured = split(left);
-      } else if (left > 9) {
-        pair[0] = createPair(left, pair);
-        return true;
-      }
-
-      if (splitOccured) return true;
-
-      if (typeof right === "object") {
-        splitOccured = split(right);
-      } else if (right > 9) {
-        pair[1] = createPair(right, pair);
-        return true;
-      }
-      return splitOccured;
-
-      function createPair(value, parent) {
-        const splitLeft = Math.floor(value / 2);
-        const splitRight = Math.round(value / 2);
-        const newPair = [splitLeft, splitRight, parent];
-        return newPair;
-      }
-    }
-
-    function checkDepth(pair, depth = 1) {
-      if (depth == 5) {
-        explode(pair);
-        return true;
-      }
-
-      const [left, right] = pair;
-      let exploded = false;
-
-      if (typeof left === "object") {
-        exploded = checkDepth(left, depth + 1);
-      }
-
-      if (exploded) return true;
-
-      if (typeof right === "object") {
-        exploded = checkDepth(right, depth + 1);
-      }
-      return exploded;
-    }
   }
 
   function addParentLinks(root) {
@@ -208,6 +207,7 @@ const readline = require("readline");
     });
   }
 
+  const numbersCopy = JSON.parse(JSON.stringify(numbers));
   numbers.forEach((pairs) => addParentLinks(pairs));
 
   const sum = sumSnailfishNumbers(numbers);
@@ -222,8 +222,33 @@ const readline = require("readline");
     return calculateMagnitude(left) * 3 + calculateMagnitude(right) * 2;
   }
 
+  function findLargestMagnitude(numbers) {
+    let largestMagnitude = 0;
+
+    for (let n1 = 0; n1 < numbers.length; n1++) {
+      for (let n2 = n1 + 1; n2 < numbers.length; n2++) {
+        let magnitude = getPairMagnitude(numbers, n1, n2);
+        if (magnitude > largestMagnitude) largestMagnitude = magnitude;
+
+        magnitude = getPairMagnitude(numbers, n2, n1);
+        if (magnitude > largestMagnitude) largestMagnitude = magnitude;
+      }
+    }
+    return largestMagnitude;
+
+    function getPairMagnitude(numbers, n1, n2) {
+      const number1 = JSON.parse(JSON.stringify(numbers[n1]));
+      const number2 = JSON.parse(JSON.stringify(numbers[n2]));
+      addParentLinks(number1);
+      addParentLinks(number2);
+
+      const sum = sumNumbers(combineNumbers(number1, number2));
+      return calculateMagnitude(sum);
+    }
+  }
+
   let resultPart1 = calculateMagnitude(sum);
-  let resultPart2 = 0;
+  let resultPart2 = findLargestMagnitude(numbersCopy);
 
   fs.writeFileSync(
     "data.out",
